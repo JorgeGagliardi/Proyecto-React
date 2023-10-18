22b855d27d402faae8fb11a04d9a1f12d1a9700a
Jorge <infocemif@gmail.com>
2023-10-18 08:59:46 -0300

    Actualización para la primera entrega del proyecto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './components/Navbar/Navbar';
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <ItemListContainer greeting="Acá van los artículos de la tienda" />
     </div>
     );
 }
